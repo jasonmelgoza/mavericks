79fa6439214a572b6b5ebadc7874a33e35b2eba1
Jason Melgoza <jasonmelgoza@gmail.com>
2022-03-10 11:32:41 -0800

    Update Dropdown.js

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -11,7 +11,30 @@ const NavButton = styled("button", {
   justifyContent: "center",
   height: "40px",
   width: "36px",
-  background: "red",
+  color: "$fg",
+  backgroundColor: "transparent",
+  borderRadius: "6px",
+
+  transition: "color 0.15s ease, background-color 0.15s ease",
+
+  "&:hover": {
+    color: "$primary",
+    backgroundColor: "$primary16",
+  },
+  "&:focus": { boxShadow: `0 0 0 2px black` },
+});
+
+const scaleIn = keyframes({
+  "0%": { opacity: 0, transform: "translateY(2px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" },
+});
+
+const StyledContent = styled(DropdownMenu.Content, {
+  transformOrigin: "var(--radix-dropdown-menu-content-transform-origin)",
+  animation: `${scaleIn} 0.5s ease-out forwards`,
+  borderRadius: "6px",
+  boxShadow: `0 0 0 2px black`,
+  padding: "12px",
 });
 
 export default () => (
@@ -22,7 +45,7 @@ export default () => (
       </NavButton>
     </DropdownMenu.Trigger>
 
-    <DropdownMenu.Content>
+    <StyledContent>
       <DropdownMenu.Item>
         <Link href="/about">
           <a>Work</a>
@@ -38,6 +61,6 @@ export default () => (
           <a>About Me</a>
         </Link>
       </DropdownMenu.Item>
-    </DropdownMenu.Content>
+    </StyledContent>
   </DropdownMenu.Root>
 );
