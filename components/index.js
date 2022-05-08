46c205790bcb73886576fd5adcfc3dfb24c63582
Jason Melgoza <jasonmelgoza@gmail.com>
2022-05-07 18:16:24 -0700

    Added Container component

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
+export { default as Container } from "./Container";
 export { default as Divider } from "./Divider";
 export { default as Dropdown } from "./Dropdown"
 export { default as Footer } from "./Footer";
