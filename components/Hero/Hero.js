dc1071bca0a0f743f2536382c77c8365be20e94b
Jason Melgoza <jasonmelgoza@gmail.com>
2022-05-14 04:10:04 +0000

    Drafted Texture for Hero element

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,8 @@
 import { styled } from "../../stitches.config";
 
 const StyledHero = styled("section", {
+  position: "relative",
+  overflow: "hidden",
   backgroundColor: "$fg8",
   display: "flex",
   flexDirection: "row",
@@ -47,12 +49,41 @@ const HeroCaption = styled("div", {
   width: "1px"
 });
 
+const StyledBackground = styled("svg", {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  zIndex: "-1",
+  width: "100%",
+  height: "calc(100vh + 200px)",
+  opacity: "0.25",
+  pointerEvents: "none",
+  transform: "translateY(0px)",
+  filter: "contrast(120%) brightness(120%)"
+});
+
+const BgTexture = () => (
+  <StyledBackground>
+    <filter id="noise">
+      <feTurbulence
+        type="fractalNoise"
+        baseFrequency=".8"
+        numOctaves="4"
+        stitchTiles="stitch"
+      ></feTurbulence>
+      <feColorMatrix type="saturate" values="0"></feColorMatrix>
+    </filter>
+    <rect width="100%" height="100%" filter="url(#noise)"></rect>
+  </StyledBackground>
+);
+
 export default function Hero({ caption, children, ...props }) {
   return (
     <>
       <StyledHero {...props}>
         <HeroCaption>{caption}</HeroCaption>
         <HeroContainer>{children}</HeroContainer>
+        <BgTexture />
       </StyledHero>
     </>
   );
