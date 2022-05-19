ba39c588ad4deb391e4c2b1cdd4f9a9d5bae2b2b
Jason Melgoza <jasonmelgoza@gmail.com>
2022-05-19 03:05:06 +0000

    Update Layout.js with meta tags

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -15,12 +15,9 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>Jason Melgoza</title>
-        <meta
-          name="description"
-          content="I'm product designer focusing on visual design and design systems"
-        />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="preload"
@@ -39,6 +36,29 @@ export default function Layout({ children }) {
           href="/fonts/Satoshi-Black.ttf"
           as="font"
           crossOrigin=""
+        />
+        <meta
+          content="Jason Melgoza — I'm product designer focusing on visual design and design systems"
+          name="description"
+        />
+        <meta name="author" content="Jason Melgoza" />
+        <meta content="website" property="og:type" />
+        <meta content="https://www.jmelgoza.com/" property="og:url" />
+        <meta
+          content="Jason Melgoza — Interface Designer"
+          property="og:site_name"
+        />
+        <meta
+          content="Jason Melgoza — Interface Designer"
+          property="og:title"
+        />
+        <meta
+          content="Jason Melgoza — I'm product designer focusing on visual design and design systems"
+          property="og:description"
+        />
+        <meta
+          content="product designer, designer, interface, ui design, ux design, design systems, minimal, jasonmelgoza, jason melgoza"
+          name="keywords"
         />
       </Head>
       <Header />
