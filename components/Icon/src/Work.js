882998e51d1f7cf36237db569f52116f646317e9
Jason Melgoza <jasonmelgoza@gmail.com>
2022-11-14 10:44:50 -0800

    Updated work icon

--- components/Icon/src/Work.js
+++ components/Icon/src/Work.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from 'react'
 
 export default function SvgWork(props) {
   return (
@@ -10,11 +10,14 @@ export default function SvgWork(props) {
       {...props}
     >
       <path
-        d="M21 14.255A23.93 23.93 0 0112 16c-3.183 0-6.22-.62-9-1.745M16 7V5a2 2 0 00-2-2h-4a2 2 0 00-2 2v2h8zM5 21h14a2 2 0 002-2V9a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-        stroke="currentColor"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        d="M16 3a1 1 0 0 1 1 1v3h-2V5.25a.25.25 0 0 0-.25-.25h-5.5a.25.25 0 0 0-.25.25V7H7V4a1 1 0 0 1 1-1h8Z"
+        fill="currentColor"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21 10a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-9Zm-2.5 0a.5.5 0 0 1 .5.5V12H5v-1.5a.5.5 0 0 1 .5-.5h13ZM10 14H5v4.5a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5V14h-5v1a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-1Z"
+        fill="currentColor"
       />
     </svg>
   )
