8cc11786a8dfd34ebf81a5807033b0e67ee28b29
Jason Melgoza <jasonmelgoza@gmail.com>
2023-06-21 22:51:27 +0000

    Added Stitches and removed old styles

---
+++ src/pages/index.md
@@ -0,0 +1,8 @@
+---
+title: Home page
+description: home with Markdoc
+---
+
+# Home
+
+This is home.
---
+++ stitches.config.js
@@ -0,0 +1,87 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme
+} = createStitches({
+  theme: {
+    colors: {
+      hiContrast: "#000",
+      loContrast: "#555",
+      bg: "white",
+      secondaryBg: "#eee"
+    }
+  },
+  media: {
+    bp1: "(min-width: 640px)",
+    bp2: "(min-width: 768px)",
+    bp3: "(min-width: 1024px)"
+  }
+});
+
+const globalStyles = globalCss({
+  "*, *::before, *::after": {
+    boxSizing: "border-box"
+  },
+
+  "*": { margin: 0 },
+
+  "html, body": {
+    height: "100%"
+  },
+
+  body: {
+    lineHeight: 1.5,
+    "-webkit-font-smoothing": "antialiased"
+  },
+
+  "img, picture, video, canvas, svg": {
+    display: "block",
+    maxWidth: "100%"
+  },
+
+  "input, button, textarea, select": {
+    font: "inherit"
+  },
+
+  "p, h1, h2, h3, h4, h5, h6": {
+    overflowWrap: "break-word"
+  },
+
+  a: {
+    color: "inherit",
+    textDecoration: "none"
+  },
+
+  p: {
+    fontSize: "$normal",
+    fontWeight: "$1",
+    lineHeight: "$2",
+
+    "&:not(:last-child)": {
+      marginBottom: "calc(1em / 0.75)"
+    },
+
+    "@bp1": {
+      fontSize: "$medium"
+    }
+  },
+
+  "#__next": {
+    isolation: "isolate"
+  },
+
+  body: {
+    fontFamily: "system-ui, sans-serif",
+    color: "$hiContrast",
+    backgroundColor: "$bg"
+  }
+});
+
+globalStyles();
