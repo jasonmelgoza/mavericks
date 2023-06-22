8606d9a4b2edb6476688d2b6e003513fcd455e8e
Jason Melgoza <jasonmelgoza@gmail.com>
2023-06-22 03:51:56 +0000

    Created Box and Block components

---
+++ src/components/Block/Block.js
@@ -0,0 +1,21 @@
+import Box from "../Box";
+
+export default function Block({ as, children, className }) {
+  return (
+    <Box
+      as={as}
+      className={className}
+      css={{
+        marginBottom: "1rem",
+        backgroundColor: "$bg",
+
+        "&.content-block": {
+          margin: "0 auto",
+          maxWidth: "656px"
+        }
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
---
+++ src/components/Box/Box.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { styled } from '../../stitches.config'
+
+const StyledBox = styled('div', {
+  margin: 0,
+  padding: 0,
+  border: 0,
+  boxSizing: 'border-box',
+  fontSize: '100%',
+  font: 'inherit',
+  verticalAlign: 'baseline',
+  WebkitTapHighlightColor: 'transparent',
+});
+
+export default function Box({ children, ...props }) {
+  return (
+    <StyledBox {...props}>
+      {children}
+    </StyledBox>
+  )
+}
