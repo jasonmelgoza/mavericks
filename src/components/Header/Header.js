a33cf8a0ecc03514d26092974b803975d213dd63
Jason Melgoza <jasonmelgoza@gmail.com>
2023-07-16 20:49:02 +0000

    Started nav style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import cx from 'clsx'
 import styles from './Header.module.scss'
 
 export default function Header() {
-
   // Using `useRouter` for rendering depending on the page's route.
   const router = useRouter()
   const path = router?.asPath
@@ -12,14 +11,30 @@ export default function Header() {
   if (path === '/') {
     return (
       <header className={cx(styles.root, styles.home)}>
-        <Link className='logo' href='/'>JASON MELGOZA</Link>
+        <Link className='logo' href='/'>
+          JASON MELGOZA
+        </Link>
       </header>
     )
   }
   return (
     <header className={cx(styles.root, styles.page)}>
-      <Link className='logo' href='/'>JASON MELGOZA</Link>
-      <nav>foo</nav>
+      <Link className='logo' href='/'>
+        JASON MELGOZA
+      </Link>
+      <nav className={styles.nav}>
+        <ul className='nav-list' >
+          <li>
+            <Link href='/about'>About</Link>
+          </li>
+          <li>
+            <Link href='/work'>Work</Link>
+          </li>
+          <li>
+            <Link href='/projects'>Projects</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
