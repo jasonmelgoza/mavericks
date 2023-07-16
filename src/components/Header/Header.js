05f58b2e9c79346a8df5f84d01c955c53304f1b9
Jason Melgoza <jasonmelgoza@gmail.com>
2023-07-16 08:35:51 -0700

    Modified header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,24 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import cx from 'clsx'
 import styles from './Header.module.scss'
 
-export default function Layout() {
-  return (
-    <header className={cx(styles.root)}>
-      Jason Melgoza
-    </header>
-  );
+export default function Header() {
+  const router = useRouter()
+  const path = router?.asPath // URL from router.
+
+  if (path === '/') {
+    return (
+      <header className={cx(styles.root, 'home')}>
+        <Link className='logo' href='/'>JASON MELGOZA</Link>
+      </header>
+    )
+  } else {
+    return (
+      <header className={cx(styles.root)}>
+        <Link className='logo' href='/'>JASON MELGOZA</Link>
+        <nav>foo</nav>
+      </header>
+    )
+  }
 }
