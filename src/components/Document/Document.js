8c420529a20b04bf1a4e563807ccacfdfd659622
Jason Melgoza <jasonmelgoza@gmail.com>
2023-07-17 09:13:57 -0700

    Moved index nav from block to document component

--- src/components/Document/Document.js
+++ src/components/Document/Document.js
@@ -1,5 +1,31 @@
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 import styles from './Document.module.scss'
 
 export default function Document({ children }) {
-  return <article className={styles.root}>{children}</article>;
+  // Using `useRouter` for rendering depending on the page's route.
+  const router = useRouter()
+  const path = router?.asPath
+
+  if (path === '/') {
+    return (
+      <>
+        <article className={styles.root}>{children}</article>
+        <nav className={styles.nav}>
+          <ul className='nav-list' >
+            <li>
+              <Link href='/work'>Work</Link>
+            </li>
+            <li>
+              <Link href='/projects'>Projects</Link>
+            </li>
+            <li>
+              <Link href='/about'>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </>
+    )
+  }
+  return <article className={styles.root}>{children}</article>
 }
--- src/pages/index.md
+++ src/pages/index.md
@@ -6,9 +6,3 @@ description: Homepage
 {% block className="hero" %}
 Product designer at SecureDocs, Inc. interface tinkerer, icon illustrator, and defender of the universe. 🤘
 {% /block %}
-
-{% block className="nav" %}
-- [Work](/work)
-- [Projects](/projects)
-- [About](/about)
-{% /block %}
