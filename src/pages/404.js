73ecb2e169223f00f275fe8154b679706b8c4d84
Jason Melgoza <jasonmelgoza@gmail.com>
2023-07-17 15:33:11 -0700

    Added 404 styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,8 @@ import styles from '@/styles/404.module.scss'
 export default function Custom404() {
   return (
     <div className={styles.root}>
-      <span>404</span>
+      <h1>404</h1>
+      <p>This page could not be found.</p>
     </div>
   )
 }
