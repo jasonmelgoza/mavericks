101fba5f7d4d79d46668581bb26f2a53e15ef8d2
Jason Melgoza <jasonmelgoza@gmail.com>
2023-08-18 06:41:39 -0700

    Removed Box component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import cx from 'clsx'
-import { Box } from '@/components'
 import { Header } from '@/components'
 import styles from './Layout.module.scss'
 
 export default function Layout({ children, className }) {
   return (
-    <Box as='main' className={cx(styles.root, className)}>
+    <main className={cx(styles.root, className)}>
       <Header />
       {children}
-    </Box>
+    </main>
   );
 }
