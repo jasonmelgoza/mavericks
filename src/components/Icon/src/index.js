c7e1f5bd4db3f30f394b7bb7395bcb958676ad5d
Jason Melgoza <jasonmelgoza@gmail.com>
2024-01-11 06:47:27 +0000

    feat: Add FileTextIcon component, update IconGithub styling, update IconLinkedin styling, update about page title and description, update style guide image paths, add Mastodon social icon to style guide and add resume link to work page

--- src/components/Icon/src/index.js
+++ src/components/Icon/src/index.js
@@ -1,4 +1,2 @@
 export { default as Dribbble } from './IconDribbble'
-export { default as Github } from './IconGithub'
-export { default as Linkedin } from './IconLinkedin'
 export { default as Mastodon } from './IconMastodon'
