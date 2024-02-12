ecb102ca9774c3877f8c469a418e60839fc86c0c
Jason Melgoza <jasonmelgoza@gmail.com>
2024-02-12 17:44:33 +0000

    Updated Pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -6,8 +6,18 @@ import styles from './Pagination.module.scss'
 export default function Pagination({ children, className }) {
   return (
     <Box className={cx(styles.root, className)}>
-      <Link href='/projects/basis'>
-        <div className='pagination-lead'>Next <Icon name='arrow-right' /></div>
+      <Link href='/projects/basis' className='pagination-prev'>
+        <div className='pagination-lead'>
+          <Icon name='arrow-left' />
+          <span>Previous</span>
+        </div>
+        <div className='pagination-description'>{children}</div>
+      </Link>
+      <Link href='/projects/basis' className='pagination-next'>
+        <div className='pagination-lead'>
+          <span>Next</span>
+          <Icon name='arrow-right' />
+        </div>
         <div className='pagination-description'>{children}</div>
       </Link>
     </Box>
