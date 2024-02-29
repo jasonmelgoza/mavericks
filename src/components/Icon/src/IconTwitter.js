5d664ed2b9d06210c4fc2032f744422b48f34c7d
Jason Melgoza <jasonmelgoza@gmail.com>
2024-02-28 19:47:51 -0800

    Added link to X
    But i'm still calling it Twitter

---
+++ src/components/Icon/src/IconTwitter.js
@@ -0,0 +1,15 @@
+const IconTwitter = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={15}
+    height={15}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M11.812 1h2.301L9.087 6.743 15 14.56h-4.628L6.744 9.82l-4.146 4.74H.293l5.375-6.145L0 1h4.746l3.276 4.332L11.812 1Zm-.808 12.184h1.274L4.051 2.304H2.683l8.32 10.88Z"
+    />
+  </svg>
+)
+export default IconTwitter
--- src/components/Icon/src/index.js
+++ src/components/Icon/src/index.js
@@ -1,2 +1,3 @@
 export { default as Dribbble } from './IconDribbble'
 export { default as Mastodon } from './IconMastodon'
+export { default as Twitter } from './IconTwitter'
