7829173b2a6ca70f3ea578297c7c38f96325ee69
Jason Melgoza <jasonmelgoza@gmail.com>
2024-04-23 17:17:26 -0700

    Update Icon component to include new 'read-cv' icon and style to rotate 'read-cv' icon. (#76)

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,4 +1,5 @@
 import cx from 'clsx'
+
 import {
   ArrowLeftIcon,
   ArrowRightIcon,
@@ -7,10 +8,13 @@ import {
   FileTextIcon,
   GitHubLogoIcon,
   LinkedInLogoIcon,
+  ReaderIcon
 } from '@radix-ui/react-icons'
 
+// Importing specific icons from the local source
 import { Dribbble, Mastodon, Twitter } from './src'
 
+// Mapping icon names to their corresponding components
 const IconMap = {
   'arrow-left': ArrowLeftIcon,
   'arrow-right': ArrowRightIcon,
@@ -21,11 +25,18 @@ const IconMap = {
   github: GitHubLogoIcon,
   linkedin: LinkedInLogoIcon,
   mastodon: Mastodon,
-  twitter: Twitter
+  twitter: Twitter,
+  'read-cv': ReaderIcon,
 }
 
+// Exporting a default Icon component
 export default function Icon({ className, name, ...props }) {
+  // Getting the appropriate icon component from the IconMap
   const IconWrapper = IconMap[name]
+  
+  // Creating a string of classnames using the clsx library
   const classNames = cx('icon', name, className)
+  
+  // If the IconWrapper exists, render it with the given classnames and props, otherwise render null
   return IconWrapper ? <IconWrapper className={classNames} {...props} /> : null
 }
