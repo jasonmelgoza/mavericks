0f91225e7371e1f5a5e9581bb1a2b68006fd70e3
Jason Melgoza <jasonmelgoza@gmail.com>
2024-06-04 15:45:34 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -59,6 +59,7 @@ export default function App({ Component, pageProps, ...props }) {
           <meta property='og:image:type' content='image/png' />
           <meta property='og:image:width' content='400' />
           <meta property='og:image:height' content='200' />
+          <meta property="twitter:image" content='/og-image.png' />
           <title>{title}</title>
           <link rel='icon' href='/favicon.ico' />
         </Head>
@@ -83,6 +84,7 @@ export default function App({ Component, pageProps, ...props }) {
         <meta property='og:image:type' content='image/png' />
         <meta property='og:image:width' content='400' />
         <meta property='og:image:height' content='200' />
+        <meta property="twitter:image" content='/og-image.png' />
         {isLanding ? (
           <title>{TITLE}</title>
         ) : (
