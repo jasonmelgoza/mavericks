2f7a68d0c5667e9c0990843d953903a702c8e994
Jason Melgoza <jasonmelgoza@gmail.com>
2024-07-24 13:36:08 -0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <script src="https://cdn.seline.so/seline.js" async />
+      </Head>
       <body>
         <Main />
         <NextScript />
