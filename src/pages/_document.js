41363b552f0ce2260fa7edd0aca8cbcf78a92a2b
Jason Melgoza <jasonmelgoza@gmail.com>
2024-07-24 13:42:32 -0700

    Added seline token

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <script src="https://cdn.seline.so/seline.js" async />
+        <script data-token="b9dfc5405912db0" src="https://cdn.seline.so/seline.js" async />
       </Head>
       <body>
         <Main />
